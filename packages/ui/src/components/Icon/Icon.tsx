cb525e8f88d4a5ec23b6f82c377fe8b38a931bfc
justn-hyeok <justn.hyeok@gmail.com>
2025-06-08 14:11:58 +0900

    feat(#7): 접근성(a11y) 개선

--- packages/ui/src/components/Icon/Icon.tsx
+++ packages/ui/src/components/Icon/Icon.tsx
@@ -4,12 +4,22 @@ import type { IconProps, StyledIconProps } from './types';
 
 export const Icon = ({
   name,
+  'aria-label': ariaLabel,
+  'aria-hidden': ariaHidden,
   size = 'medium',
   weight = 'regular',
   fill = 'regular',
   ...props
 }: IconProps) => (
-  <StyledIcon size={size} weight={weight} fill={fill} {...props}>
+  <StyledIcon
+    size={size}
+    weight={weight}
+    fill={fill}
+    role={ariaLabel ? 'img' : undefined}
+    aria-label={ariaLabel}
+    aria-hidden={ariaHidden ?? (!ariaLabel ? 'true' : undefined)}
+    {...props}
+  >
     {name}
   </StyledIcon>
 );
