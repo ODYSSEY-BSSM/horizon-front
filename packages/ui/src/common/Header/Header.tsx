dccbc3ccb3fdadf8bd31e79f5e2cb29b410ad2e4
sunz8281 <sunz8281@gmail.com>
2025-06-10 22:06:52 +0900

    fix(#12): 헤더 화살표 버튼 삭제

--- packages/ui/src/common/Header/Header.tsx
+++ packages/ui/src/common/Header/Header.tsx
@@ -1,7 +1,5 @@
 import styled from '@emotion/styled';
 import { colors } from '@horizon/design-system/src';
-import { Row } from '../../components/Flex/Row';
-import { Icon } from '../../components/Icon/Icon';
 import { Text } from '../../components/Text/Text';
 
 interface HeaderProps {
@@ -11,21 +9,7 @@ interface HeaderProps {
 export const Header = ({ title }: HeaderProps) => {
   return (
     <StyledHeader>
-      <Row gap="20px" alignItems="center">
-        <Row>
-          <Icon
-            name="chevron_left"
-            style={{ color: colors.grayscale['200'] }}
-            size="large"
-          />
-          <Icon
-            name="chevron_right"
-            style={{ color: colors.grayscale['200'] }}
-            size="large"
-          />
-        </Row>
-        <Text variant="T_SB_16">{title}</Text>
-      </Row>
+      <Text variant="T_SB_16">{title}</Text>
     </StyledHeader>
   );
 };
