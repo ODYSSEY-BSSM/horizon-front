93a2fc138255d5021317149681fdfd49161da675
justn-hyeok <justn.hyeok@gmail.com>
2025-06-19 01:05:11 +0900

    chore(#33): react-router-dom 삭제 및 빌드 오류 수정

--- landing/src/app/page.tsx
+++ landing/src/app/page.tsx
@@ -2,10 +2,12 @@
 
 import React from 'react';
 import { motion } from 'framer-motion';
+import { AppLayout, Button } from '@odyssey-horizon/ui';
 
 export default function Home(): React.JSX.Element {
   return (
     <main>
+      <AppLayout title="Horizon" children={<></>} />
       <motion.div
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
@@ -13,6 +15,7 @@ export default function Home(): React.JSX.Element {
       >
         <h1>Horizon</h1>
         <p>Odyssey의 프로젝트</p>
+        <Button text="시작하기" type="contained" size="large" />
       </motion.div>
     </main>
   );
